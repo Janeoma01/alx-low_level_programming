275ae69e640622bb148636acac2008c753d3a052
Janeoma01 <janeoma222@gmail.com>
2023-03-15 14:56:21 -0700

    lib

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,18 +1,17 @@
 #include "main.h"
+
 /**
- * _strlen - returns the length of a string
- * @s: string
- * Return: length
+ *_strlen -  returns the length of a string.
+ *@s: string
+ *
+ *Return: string len
  */
 int _strlen(char *s)
 {
-	int longi = 0;
+	int len = 0;
 
-	while (*s != '\0')
-	{
-		longi++;
-		s++;
-	}
+	while (*(s + len) != '\0')
+		++len;
 
-	return (longi);
+	return (len);
 }
